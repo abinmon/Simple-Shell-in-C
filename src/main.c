b78dffb646ab198084984c4e0e361758db47ac7c
Manjinder <manjinder.singh.2018@strath.ac.uk>
2020-01-30 11:38:01 +0000

    Init Config

---
+++ src/main.c
@@ -0,0 +1,12 @@
+/* Main File */
+#include <stdio.h>
+#include <stdlib.h>
+#include "simpleShell.h"
+#define ARG_MAX 32000
+
+int main(void) {
+	char commands[ARG_MAX];
+	init();
+	readInput(commands);
+	exit(0);
+}
---
+++ src/simpleShell.c
@@ -0,0 +1,25 @@
+#include "simpleShell.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void init() {
+	char* username = getenv("USERNAME");
+	printf("\nuser@%s", username);
+	printf("\n");
+	printf("$ ");
+}
+
+void readInput(char* commands) {
+	gets(commands);
+  char *cmd;
+
+  cmd = strtok(commands, " ");
+  while( cmd != NULL ) {
+  	printf("%s\n", cmd);
+
+		cmd = strtok(NULL, " ");
+  }
+
+
+}
