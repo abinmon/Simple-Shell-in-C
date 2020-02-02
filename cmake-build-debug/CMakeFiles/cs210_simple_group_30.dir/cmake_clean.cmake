fe5417b8506af995d8adaf55e203d05bc8f656e0
wdb18170 <wdb18170@uni.strath.ac.uk>
2020-02-02 18:51:39 +0000

    Revert "Fixed Leading Spaces When Exit Was Entered"
    This reverts commit 5f2f56c4d35e5155f2ef1edaa330e22ac60b1fe5.

---
+++ cmake-build-debug/CMakeFiles/cs210_simple_group_30.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cs210_simple_group_30.dir/src/main.c.o"
+  "CMakeFiles/cs210_simple_group_30.dir/src/simpleShell.c.o"
+  "cs210_simple_group_30.pdb"
+  "cs210_simple_group_30"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cs210_simple_group_30.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
